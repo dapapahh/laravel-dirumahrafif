80ac1460c362dec5e8e49c1c36f4c2aa2c1a30f9
dahrul <dahruloelum@gmail.com>
2026-01-02 09:43:31 +0700

    from bapak

--- app/Models/section.php
+++ app/Models/section.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class section extends Model
+class Section extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','slug','thumbhnail','content','post_as'];
+    protected $fillable = ['title','slug','thumbnail','content','post_as'];
 }
